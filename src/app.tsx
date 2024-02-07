7c971c47e276e64b21edffb50f6e6c15e237f994
rom013 <romullomelo0130@gmail.com>
2024-02-07 11:01:31 -0300

    feat: create modal and toasts

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,6 @@ import Logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
-
-
 export function App() {
 	return (
 		<div className="mx-auto max-w-6xl my-12 space-y-6">
