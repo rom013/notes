c3bc7622ad890c62d0eee7b0e2a93e03226c0193
rom013 <romullomelo0130@gmail.com>
2024-02-08 07:53:45 -0300

    feat: search

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import Logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
@@ -11,27 +11,40 @@ interface Note {
 
 export function App() {
 
-	const [notes, setNotes] = useState<Note[]>(()=>{
+	const [search, setSearch] = useState<string>('')
+
+	const [notes, setNotes] = useState<Note[]>(() => {
 		const noteOnStorage = localStorage.getItem('notes')
 
-		if (noteOnStorage) return JSON.parse(noteOnStorage) 
-		
+		if (noteOnStorage) return JSON.parse(noteOnStorage)
+
 		return []
 	})
 
-	function onNoteCreated(content: string){
+	function onNoteCreated(content: string) {
 		const newNote = {
 			id: crypto.randomUUID(),
 			date: new Date(),
 			content
 		}
 
-		const notesArray = [ newNote, ...notes ]
+		const notesArray = [newNote, ...notes]
 
 		setNotes(notesArray)
 
 		localStorage.setItem('notes', JSON.stringify(notesArray))
 	}
+
+	function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+		const query = event.target.value
+
+		setSearch(query)
+	}
+
+	const filteredNotes = search !== ''
+		? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase().trim()))
+		: notes
+
 
 	return (
 		<div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -43,7 +56,7 @@ export function App() {
 					type="text"
 					placeholder="Busque em suas notas..."
 					className="w-full bg-transparent text-3xl text-slate-300 font-semibold tracking-tight placeholder:text-slate-500 outline-none"
-
+					onChange={handleSearch}
 				/>
 			</form>
 
@@ -55,9 +68,9 @@ export function App() {
 				<NewNoteCard onNoteCreated={onNoteCreated} />
 
 				{
-					notes.map(note => {
+					filteredNotes.map(note => {
 						return <NoteCard
-							note={ note }
+							note={note}
 							key={note.id}
 						/>
 					})
